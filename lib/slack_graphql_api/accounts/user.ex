3253de10be8181d12a7ad67e41cf86054c53f3ea
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-10 13:59:26 +0100

    add validation to registration changeset

--- lib/slack_graphql_api/accounts/user.ex
+++ lib/slack_graphql_api/accounts/user.ex
@@ -1,10 +1,10 @@
 defmodule SlackGraphqlApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias SlackGraphqlApi.Accounts.User
 
   schema "users" do
-    field :user_name, :string
+    field :user_name, :string, unique: true
     field :email, :string, unique: true
     field :first_name, :string
     field :last_name, :string
@@ -18,9 +18,30 @@ defmodule SlackGraphqlApi.Accounts.User do
   end
 
   @doc false
-  def changeset(user, attrs) do
+  # def changeset(user, attrs) do
+  #   user
+  #   |> cast(attrs, [:first_name, :last_name, :user_name, :email, :password_hash, :role, :reset_password_token, :reset_password_sent_at])
+  #   |> validate_required([:first_name, :last_name, :user_name, :email, :password_hash, :role, :reset_password_token, :reset_password_sent_at])
+  # end
+
+  def registration_changeset(%User{} = user, params \\ %{}) do
     user
-    |> cast(attrs, [:first_name, :last_name, :user_name, :email, :password_hash, :role, :reset_password_token, :reset_password_sent_at])
-    |> validate_required([:first_name, :last_name, :user_name, :email, :password_hash, :role, :reset_password_token, :reset_password_sent_at])
+    |> cast(params, [:email, :first_name, :last_name, :password, :user_name])
+    |> validate_required([:email, :first_name, :last_name, :password, :role, :user_name])
+    |> unique_constraint(:user_name)
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &(String.downcase(&1)))
+    |> unique_constraint(:email)
+    |> validate_length(:password, min: 6, max: 100)
+    |> hash_password()
+  end
+
+  defp hash_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :password_hash, Comeonin.Argon2.hashpwsalt(password))
+      _ ->
+        changeset
+    end
   end
 end
