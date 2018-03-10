5136f3254ac8907fb08619b7562260dadd3ec5c5
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-10 13:57:03 +0100

    add create user mutation

--- lib/slack_graphql_api_web/resolvers/user_resolver.ex
+++ lib/slack_graphql_api_web/resolvers/user_resolver.ex
@@ -4,4 +4,9 @@ defmodule SlackGraphqlApiWeb.Resolvers.UserResolver do
     def users(_,_,_) do
         {:ok, Accounts.list_users}
     end
+
+    def create_user(_,%{input: input},_) do
+        IO.inspect(input)
+        Accounts.create_user(input)
+    end
 end
