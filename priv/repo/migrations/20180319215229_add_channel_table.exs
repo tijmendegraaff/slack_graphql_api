c1fb7116cbc4a2e6e4c59ffaa95c807d3ea2274b
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-19 23:42:10 +0100

    generate migration

---
+++ priv/repo/migrations/20180319215229_add_channel_table.exs
@@ -0,0 +1,14 @@
+defmodule SlackGraphqlApi.Repo.Migrations.AddChannelTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:channels) do
+      add :name, :string
+      add :is_public, :boolean, default: true, null: false
+      add :user_id, references(:users, on_delete: :nothing)
+      add :team_id, references(:teams, on_delete: :nothing)
+
+      timestamps()
+    end
+  end
+end
