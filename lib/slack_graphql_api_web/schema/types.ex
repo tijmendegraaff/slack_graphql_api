06bfe86e3afb23ba2895d9ea6a382a920ad785f7
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-19 23:43:47 +0100

    add channel type

--- lib/slack_graphql_api_web/schema/types.ex
+++ lib/slack_graphql_api_web/schema/types.ex
@@ -7,5 +7,7 @@ defmodule SlackGraphqlApiWeb.Schema.Types do
     import_types Types.UserType
     import_types Types.SessionType
     import_types Types.TeamType
+    import_types Types.ChannelType
+    
 
   end
