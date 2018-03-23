26091f9bf916c3d2762bd09fa09532e177c45074
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-24 00:12:25 +0100

    fix type in patternmatch

--- lib/slack_graphql_api_web/resolvers/channel_resolver.ex
+++ lib/slack_graphql_api_web/resolvers/channel_resolver.ex
@@ -2,7 +2,7 @@ defmodule SlackGraphqlApiWeb.Resolvers.ChannelResolver do
     alias SlackGraphqlApi.Messenger
 
     def create_channel(_,%{input: input},%{context: %{user: user}}) do
-        args = Map.merge(input, %{user_id: user.id})
+        args = Map.merge(input, %{sender_id: user.id})
         Messenger.create_channel(args)
     end
 
