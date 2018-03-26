4a6e34b5a91b4583ddd9427532e2350575473bf2
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-26 03:54:55 +0200

    fix add private channel inputtype

--- lib/slack_graphql_api_web/resolvers/channel_resolver.ex
+++ lib/slack_graphql_api_web/resolvers/channel_resolver.ex
@@ -1,5 +1,6 @@
 defmodule SlackGraphqlApiWeb.Resolvers.ChannelResolver do
   alias SlackGraphqlApi.Messenger
+  import Ecto
 
   def create_channel(_, %{input: input}, %{context: %{user: user}}) do
     args = Map.merge(input, %{user_id: user.id})
@@ -10,7 +11,15 @@ defmodule SlackGraphqlApiWeb.Resolvers.ChannelResolver do
   def create_direct_message_channel(_, %{input: input}, %{context: %{user: user}}) do
     new_memberlist = [user.id | input.members]
     x = Map.put(input, :members, new_memberlist)
-    args = Map.merge(x, %{is_direct_message_channel: true, user_id: user.id, is_public: false})
+
+    args =
+      Map.merge(x, %{
+        is_direct_message_channel: true,
+        user_id: user.id,
+        is_public: false
+      })
+
+    IO.inspect(args)
     {:ok, Messenger.create_direct_message_channel(args)}
   end
 end
