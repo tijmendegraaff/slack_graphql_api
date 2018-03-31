76bc42f04a3a3907ea6a03b180ab26eb0bcd91e7
Tijmen de Graaff <tijmen76@gmail.com>
2018-03-31 18:17:51 +0200

    make seperate ci config file

---
+++ config/ci.exs
@@ -0,0 +1,24 @@
+use Mix.Config
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :slack_graphql_api, SlackGraphqlApiWeb.Endpoint,
+  http: [port: 4001],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Speed up password hasing in test env
+config :argon2_elixir,
+  t_cost: 2,
+  m_cost: 12
+
+# Configure your database
+config :slack_graphql_api, SlackGraphqlApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "slack_graphql_api_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
