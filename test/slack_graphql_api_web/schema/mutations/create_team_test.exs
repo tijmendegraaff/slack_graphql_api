fdb18a93662af0e92003bc2df77dd72493174638
Tijmen de Graaff <tijmen76@gmail.com>
2018-04-07 23:01:23 +0200

    add team query to testfile

--- test/slack_graphql_api_web/schema/mutations/create_team_test.exs
+++ test/slack_graphql_api_web/schema/mutations/create_team_test.exs
@@ -2,4 +2,16 @@ defmodule SlackGraphqlApiWeb.Schema.Mutation.CreateTeamTest do
   use SlackGraphqlApiWeb.ConnCase, async: true
 
   import SlackGraphqlApi.Factory
+
+  @query """
+  mutation createUser($input: UserInputType!) {
+    createUser(input: $input){
+      id
+      name
+      description
+      avatar
+      owner
+    }
+  }
+  """
 end
